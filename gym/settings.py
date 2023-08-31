1038ae21cf6fdbca47071d648e5341c387d3831c
Bushra Nasir <137592512+BushraNasir23@users.noreply.github.com>
2023-08-30 23:43:02 -0700

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-axay8gtmtr@uqvo%&=6x*wzzho!k39)1f_vpwni(0cp0())@p_'
+SECRET_KEY = 'Bushra Nasir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
